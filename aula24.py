f5c926a25047102e163ab8fe6e32494d97e0a884
natevitor <rm550381@fiap.com.br>
2023-08-26 00:07:06 -0300

    aula24.py

--- aula24.py
+++ aula24.py
@@ -1,15 +1,15 @@
 # Operadores in e not in
 # Strings são iteráveis
 #  0 1 2 3 4 5
-#  O t á v i o
+#  V i t o r 
 # -6-5-4-3-2-1
-# nome = 'Otávio'
+# nome = 'Vitor'
 # print(nome[2])
 # print(nome[-4])
-# print('vio' in nome)
+# print('tor' in nome)
 # print('zero' in nome)
 # print(10 * '-')
-# print('vio' not in nome)
+# print('tor' not in nome)
 # print('zero' not in nome)
 
 nome = input('Digite seu nome: ')
