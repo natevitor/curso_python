bc7997cce1023d2b907137555230c28d8ef6f2b0
natevitor <rm550381@fiap.com.br>
2023-08-27 14:15:14 -0300

    aula40.py

--- aula40.py
+++ aula40.py
@@ -76,7 +76,19 @@ while True:
 
     if len (operador) > 1:
         print('Digite apenas um numero.')
-        continue                
+        continue       
+
+    print('Realizando sua conta. Confira o resultado abaixo')  
+    if operador == '+':
+        print(f'{num_1_float} + {num_2_float}=', num_1_float + num_2_float)   
+    elif operador =='-':
+        print(f'{num_1_float} * {num_2_float}=', num_1_float - num_2_float)   
+    elif operador == '/':
+        print(f'{num_1_float} / {num_2_float}=', num_1_float / num_2_float)   
+    elif operador == '*':
+        print(f'{num_1_float} * {num_2_float}=', num_1_float * num_2_float)   
+    else: 
+        print('Nunca deveria chegar aqui.')
 
 
     sair = input('Quer sair? [s]sim:').lower().startswith('s')
