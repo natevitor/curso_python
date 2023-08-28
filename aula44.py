35f4436e304ad80df677c6136c2430469f28c39a
natevitor <rm550381@fiap.com.br>
2023-08-28 18:22:39 -0300

    aula44.py

---
+++ aula44.py
@@ -0,0 +1,9 @@
+"""
+For + Range
+range -> range(start, stop, step)
+Tem palavras no Python que são reservadas e eu não consigo usar no nome de variaveis 
+"""
+numeros = range(0, 100, 8)
+
+for numero in numeros:
+    print(numero)
